76d05355fbb0f6d5a6ae496245d6fc48bea8a8a1
KarthikVelayutham <karthik.velayutham@gmail.com>
2018-06-27 00:04:30 -0500

    a small part

--- solution.py
+++ solution.py
@@ -1,6 +1,8 @@
 # This is the solution for the puzzle
 
+# First we define the board object
 class Board(object):
+	board = []
 	def __init__(rows, columns):
 		self.rows = rows
 		self.columns = columns
