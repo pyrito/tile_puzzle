5c44dc374073cf65bef4b425cad40a091b0dc08b
KarthikVelayutham <karthik.velayutham@gmail.com>
2018-06-26 23:53:09 -0500

    first commit

---
+++ cell.js
@@ -0,0 +1,40 @@
+function Cell(i, j, w, colored) {
+  this.i = i;
+  this.j = j;
+  this.x = i * w;
+  this.y = j * w;
+  this.w = w;
+
+  this.bee = false;
+  this.revealed = false;
+  this.clicked = false;
+  this.colored = colored;
+}
+
+Cell.prototype.show = function() {
+  stroke(0);
+  noFill();
+  rect(this.x, this.y, this.w, this.w);
+
+  // If the button is clicked, then we invert its color
+  if (this.colored == 0) {
+    noFill();
+    rect(this.x, this.y, this.w, this.w);
+  }
+  else if (this.colored == 1) {
+    fill(127);
+    rect(this.x, this.y, this.w, this.w);
+  }
+ }
+
+Cell.prototype.contains = function(x, y) {
+  return (x > this.x && x < this.x + this.w && y > this.y && y < this.y + this.w);
+}
+
+Cell.prototype.invertColor = function() {
+  this.colored = (this.colored == 0) ? 1 : 0;
+}
+
+Cell.prototype.isColored = function() {
+  return this.colored;
+}
---
+++ sketch.js
@@ -0,0 +1,85 @@
+
+// Create the 2D array
+function make2DArray(cols, rows) {
+  var arr = new Array(cols);
+  for (var i = 0; i < arr.length; i++) {
+    arr[i] = new Array(rows);
+  }
+  return arr;
+}
+
+var grid;
+var cols;
+var rows;
+var w = 100;
+var done = true;
+var moves = 0;
+// Setup for the canvas
+function setup() {
+  createCanvas(301, 301);
+  cols = floor(width / w);
+  rows = floor(height / w);
+  grid = make2DArray(cols, rows);
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      grid[i][j] = new Cell(i, j, w, 0);
+    }
+  }
+  //Default center is going to be black
+  grid[1][1] = new Cell(1, 1, w, 1);
+  randX = Math.floor((Math.random() * 3));
+  randY = Math.floor((Math.random() * 3));
+  grid[randX][randY] = new Cell(randX, randY, w, 1);
+
+}
+
+// Draw the canvas as well as the cells
+function draw() {
+  background(255);
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      grid[i][j].show();
+    }
+  }
+}
+
+// JavaScript function for when the mouse is pressed
+function mousePressed() {
+  if (!done) {
+    for (var i = 0; i < cols; i++) {
+      for (var j = 0; j < rows; j++) {
+        if (grid[i][j].contains(mouseX, mouseY)) {
+          grid[i][j].invertColor();
+          for (var x = -1; x < 2; x++) {
+            for (var y = -1; y < 2;  y++) {
+              if ((i + x) >= 0 && (j + y) >= 0 && (i + x) < cols && (j + y) < rows && (Math.abs(x) !=  Math.abs(y))) {
+                grid[i+x][j+y].invertColor();
+              }
+            }
+          }
+        }
+      }
+    }
+
+    moves += 1;
+    console.log(moves);
+    // Check if the board is filled or not
+  }
+}
+
+// Check if it's the end of the game
+function gameStart() {
+  done = false;
+}
+
+function checkEnd() {
+    for (var i = 0; i < cols; i++) {
+      for (var j = 0; j < rows; j++) {
+        done = true;
+        if (grid[i][j].isColored() == 0) {
+          done = false;
+          break;
+        }
+      }
+    }
+}
